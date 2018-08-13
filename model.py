Changed model to categorize MNIST images using a single fully connected layer, as in tensorflow tutorial. Fixed accuracy calculation from using running average accuracy from tf.metrics.accuracy. Tested the network initial loss and accuracy conditions and that it quickly overfit a small number of training samples.

model.py
@@ -19,7 +19,7 @@ def stable_norm_grad(x, dy):
 def stable_norm(x):
   return tf.norm(x, axis=-1)
 
-class diffractionCLASS():
+class basicNNCLASS():
   """
   modelCLASS is a parent class to neural networks that encapsulates all 
   the logic necessary for training general nerual network archetectures.
@@ -102,6 +102,7 @@ class diffractionCLASS():
     self.Nrowcol  = data["train_X"].shape[1]
     self.Noutputs = data["train_Y"].shape[-1]
     self.Nclasses = np.unique(self.data["train_Y"][:,0]).shape[0]
+    print("Nclasses", self.Nclasses)
 
     # Misc
     self.global_step = tf.Variable(0, name="global_step", trainable=False)
@@ -155,12 +156,10 @@ class diffractionCLASS():
       print("ERROR: Range of data requested is outside of data size!!!")
       sys.exit()
     
-    outDataX         = self.data[dSet+"_X"][minInd:maxInd]
-    outDataY_quality = self.data[dSet+"_Y"][minInd:maxInd,0].astype(np.int32)
-    outDataY_flt1    = np.reshape(self.data[dSet+"_Y"][minInd:maxInd,1], (-1,1))
-    outDataY_flt2    = np.reshape(self.data[dSet+"_Y"][minInd:maxInd,2], (-1,1))
+    outDataX = self.data[dSet+"_X"][minInd:maxInd]
+    outDataY = self.data[dSet+"_Y"][minInd:maxInd,0]
 
-    return outDataX, outDataY_quality, outDataY_flt1, outDataY_flt2
+    return outDataX, outDataY
 
 
 
@@ -170,15 +169,11 @@ class diffractionCLASS():
     Initialize all place holders with size variables from self.config
     """
 
-    self.inputX = tf.placeholder(name="inputFeatures", dtype=tf.float32,
+    self.inputX     = tf.placeholder(name="inputFeatures", dtype=tf.float32,
                              shape=[None, self.Nrowcol, self.Nrowcol, 1])
-    self.inputY_quality = tf.placeholder(name="inputQualityLabels", dtype=tf.int32,
+    self.inputY     = tf.placeholder(name="inputLabels", dtype=tf.int32,
                              shape=[None])
-    self.inputY_flt1    = tf.placeholder(name="inputFlt1Labels", dtype=tf.float32,
-                             shape=[None, 1])
-    self.inputY_flt2    = tf.placeholder(name="inputFlt2Labels", dtype=tf.float32,
-                             shape=[None, 1])
-    self.isTraining     = tf.placeholder(name="isTraining", dtype=tf.bool)
+    self.isTraining = tf.placeholder(name="isTraining", dtype=tf.bool)
 
   
   #############################################################################
@@ -230,60 +225,26 @@ class diffractionCLASS():
     """
 
     self.predictionCLS = predictionClass(self.FLAGS.embedding_size, self.Nclasses)
-    self.qualityPred, self.flt1Pred, self.flt2Pred\
-          = self.predictionCLS.predict(self.inputX, self.isTraining)
+    self.prediction = self.predictionCLS.predict(self.inputX, self.isTraining)
 
 
   #############################################################################
   def add_loss(self):
 
     with tf.variable_scope("loss"):
-      # Quality loss
-      self.qualityLoss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-                            logits=self.qualityPred,
-                            labels=self.inputY_quality))
-      tf.summary.scalar("qualityLoss", self.qualityLoss)
-
-      # float1 loss
-      self.flt1Loss    = tf.losses.mean_squared_error(
-                            predictions=self.flt1Pred,
-                            labels=self.inputY_flt1)
-      tf.summary.scalar("flt1Loss", self.flt1Loss)
-
-      # float2 loss
-      self.flt2Loss    = tf.losses.mean_squared_error(
-                            predictions=self.flt2Pred,
-                            labels=self.inputY_flt2)
-      tf.summary.scalar("flt2Loss", self.flt2Loss)
-
-      self.loss = self.qualityLoss + self.flt1Loss + self.flt2Loss
+      self.loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+                            logits=self.prediction,
+                            labels=self.inputY))
+      tf.summary.scalar("loss", self.loss)
 
 
   #############################################################################
   def add_accuracy(self):
 
     with tf.variable_scope("accuracy"):
-      # Quality accuracy
-      qualLogits = tf.argmax(self.qualityPred, axis=-1)
-      _,self.qualityAcc = tf.metrics.accuracy(
-                          predictions=qualLogits,
-                          labels=self.inputY_quality)
-
-      # float1 accuracy
-      self.flt1Acc    = tf.reduce_mean(
-                          tf.abs(
-                            tf.divide((self.flt1Pred - self.inputY_flt1),
-                              (self.inputY_flt1 + 1e-5))
-                          )
-                        )
-
-      # float2 accuracy
-      self.flt2Acc    = tf.reduce_mean(
-                          tf.abs(
-                            tf.divide((self.flt2Pred - self.inputY_flt2),
-                              (self.inputY_flt2 + 1e-5))
-                          )
-                        )
+      logits = tf.argmax(self.prediction, axis=-1, output_type=tf.int32)
+      compare = tf.equal(logits, self.inputY)
+      self.accuracy = tf.reduce_mean(tf.cast(compare, tf.float32))
 
 
   #############################################################################
@@ -292,9 +253,7 @@ class diffractionCLASS():
     # Build feed dictionary
     input_feed = { 
       self.inputX : batch[0], 
-      self.inputY_quality : batch[1],
-      self.inputY_flt1 : batch[2],
-      self.inputY_flt2 : batch[3],
+      self.inputY : batch[1],
       self.isTraining : True}
 
     # Output feed
@@ -334,16 +293,13 @@ class diffractionCLASS():
         "./checkpoints/", 
         sess.graph)
 
-    #if not (self.modelRestored or self.hasTrained):
-    #  sess.run(tf.global_variables_initializer())
-    #  sess.run(tf.local_variables_initializer())
-
-    # Print number of model parameters
-    tic = time.time()
-    params = tf.trainable_variables()
-    num_params = sum(map(lambda t: np.prod(tf.shape(t.value()).eval()), params))
-    toc = time.time()
-    #logging.info("Number of params: %d (retrieval took %f secs)" % (num_params, toc - tic))
+    # Print initial model predictions
+    trainLoss = self.get_loss(sess, dSet="train")
+    trainAccs = self.get_accuracy(sess, dSet="train")
+    valLoss   = self.get_loss(sess, dSet="val")
+    valAccs   = self.get_accuracy(sess, dSet="val")
+    logging.info("Initial training Loss / Accuracy: %f / %f)" % (trainLoss, trainAccs))
+    logging.info("Initial validation Loss / Accuracy: %f / %f)" % (valLoss, valAccs))
 
     Nbatches = int(np.ceil(self.data["train_X"].shape[0]/float(self.FLAGS.batch_size)))
     randomRatio = 1.0
@@ -357,9 +313,7 @@ class diffractionCLASS():
       epoch += 1
       epoch_tic = time.time()
 
-      logging.info("\n\n/////  Begin Epoch {}  /////".format(epoch))
-      if self.FLAGS.verbose :
-        print("\n\nEpoch: %i" % (epoch))
+      logging.info("\n\n/////  Begin Epoch {}  /////\n".format(epoch))
 
       #####  Loop over mini batches  #####
       for ibt in range(Nbatches):
@@ -372,15 +326,6 @@ class diffractionCLASS():
         curLoss, global_step = self.run_train_step(sess, batch, summaryWriter)
         tstep_toc = time.time()
         tstep_time = tstep_toc - tstep_tic
-
-        # Print training results
-        if global_step % self.FLAGS.print_every == 0:
-          print_info = "iter %d, epoch %d, batch %d, loss %.5f, batch time %.3f" %\
-              (global_step, epoch, ibt, curLoss, tstep_time)
-          loggin.info(print_info)
-          
-          if self.FLAGS.verbose:
-            print(print_info)
 
         # Save model
         if global_step % self.FLAGS.save_every == 0:
@@ -401,7 +346,7 @@ class diffractionCLASS():
           trainAccs = self.get_accuracy(sess, dSet="train")
 
           self.writeSummary(trainLoss, "train/loss", summaryWriter, global_step)
-          self.writeSummary(trainAccs[0], "train/acc", summaryWriter, global_step)
+          self.writeSummary(trainAccs, "train/acc", summaryWriter, global_step)
           self.history["train"].append((global_step, trainLoss, trainAccs))
 
           # Evaluate validation data
@@ -409,16 +354,13 @@ class diffractionCLASS():
           valAccs = self.get_accuracy(sess, dSet="val")
 
           self.writeSummary(valLoss, "val/loss", summaryWriter, global_step)
-          self.writeSummary(valAccs[0], "val/acc", summaryWriter, global_step)
+          self.writeSummary(valAccs, "val/acc", summaryWriter, global_step)
           self.history["val"].append((global_step, valLoss, valAccs))
 
           # Logging results
-          print_info = "%i\tTraining %.5f / %.5f / %.5f / %.5f \tValidation %.5f / %.5f / %.5f / %.5f" %\
-              (global_step, trainLoss, trainAccs[0], trainAccs[1], trainAccs[2], valLoss, valAccs[0], valAccs[1], valAccs[2])
+          print_info = "%i\tTraining %.5f / %.5f \tValidation %.5f / %.5f" %\
+              (global_step, trainLoss, trainAccs, valLoss, valAccs)
           logging.info(print_info)
-          print(print_info)
-          if self.FLAGS.verbose:
-            print(print_info)
 
           # Save best models
           if (best_val_loss is None) or (valLoss < best_val_loss):
@@ -431,10 +373,10 @@ class diffractionCLASS():
             self.saveTrainingHistory(
                     fileName=self.FLAGS.bestModel_loss_ckpt_path + "/" + self.FLAGS.experiment_name, 
                     global_step=global_step)
-          if (best_val_acc is None) or (valAccs[0] > best_val_acc):
+          if (best_val_acc is None) or (valAccs > best_val_acc):
             logging.info("Saving best accuracy model at iteration {} in {}".format(
                     global_step, self.FLAGS.bestModel_acc_ckpt_path))
-            best_val_acc = valAccs[0]
+            best_val_acc = valAccs
             self.bestAccSaver.save(sess, 
                     self.FLAGS.bestModel_acc_ckpt_path + "/" + self.FLAGS.experiment_name, 
                     global_step=global_step)
@@ -526,20 +468,14 @@ class diffractionCLASS():
 
     if (dataX is not None) and (dataY is not None):
       feed_dict[self.inputX] = dataX
-      if dataY is not None:
-        feed_dict[self.inputY_quality]  = dataY[0]
-        feed_dict[self.inputY_flt1]     = dataY[1]
-        feed_dict[self.inputY_flt2]     = dataY[2]
+      feed_dict[self.inputY] = dataY
     elif dSet is not None:
       batch = self.get_data(self.data[dSet+"_X"].shape[0], dSet)
-      feed_dict[self.inputX]          = batch[0]
-      feed_dict[self.inputY_quality]  = batch[1]
-      feed_dict[self.inputY_flt1]     = batch[2]
-      feed_dict[self.inputY_flt2]     = batch[3]
+      feed_dict[self.inputX]  = batch[0]
+      feed_dict[self.inputY]  = batch[1]
     else:
       print("ERROR: Must specify dSet or dataX and dataY for get_accuracy!!!")
       raise RuntimeError
-
    
     return sess.run(outputs, feed_dict)
 
@@ -551,21 +487,16 @@ class diffractionCLASS():
 
     if (dataX is not None) and (dataY is not None):
       feed_dict[self.inputX] = dataX
-      if dataY is not None:
-        feed_dict[self.inputY_quality]  = dataY[0]
-        feed_dict[self.inputY_flt1]     = dataY[1]
-        feed_dict[self.inputY_flt2]     = dataY[2]
+      feed_dict[self.inputY] = dataY
     elif dSet is not None:
       batch = self.get_data(self.data[dSet+"_X"].shape[0], dSet)
-      feed_dict[self.inputX]          = batch[0]
-      feed_dict[self.inputY_quality]  = batch[1]
-      feed_dict[self.inputY_flt1]     = batch[2]
-      feed_dict[self.inputY_flt2]     = batch[3]
+      feed_dict[self.inputX] = batch[0]
+      feed_dict[self.inputY] = batch[1]
     else:
       print("ERROR: Must specify dSet or dataX and dataY for get_accuracy!!!")
       raise RuntimeError
 
-    return sess.run([self.qualityPred, self.flt1Pred, self.flt2Pred], feed_dict)
+    return sess.run([self.prediction], feed_dict)
 
 
   #############################################################################
@@ -575,21 +506,16 @@ class diffractionCLASS():
 
     if (dataX is not None) and (dataY is not None):
       feed_dict[self.inputX] = dataX
-      feed_dict[self.inputY_quality]  = dataY[0]
-      feed_dict[self.inputY_flt1]     = dataY[1]
-      feed_dict[self.inputY_flt2]     = dataY[2]
+      feed_dict[self.inputY] = dataY
     elif dSet is not None:
       batch = self.get_data(self.data[dSet+"_X"].shape[0], dSet)
-      feed_dict[self.inputX]          = batch[0]
-      feed_dict[self.inputY_quality]  = batch[1]
-      feed_dict[self.inputY_flt1]     = batch[2]
-      feed_dict[self.inputY_flt2]     = batch[3]
+      feed_dict[self.inputX] = batch[0]
+      feed_dict[self.inputY] = batch[1]
     else:
       print("ERROR: Must specify dSet or dataX and dataY for get_accuracy!!!")
       raise RuntimeError
 
-
-    return sess.run([self.qualityAcc, self.flt1Acc, self.flt2Acc], feed_dict)
+    return sess.run([self.accuracy], feed_dict)[0]
 
 
   #############################################################################
@@ -599,15 +525,11 @@ class diffractionCLASS():
 
     if (dataX is not None) and (dataY is not None):
       feed_dict[self.inputX] = dataX
-      feed_dict[self.inputY_quality]  = dataY[0]
-      feed_dict[self.inputY_flt1]     = dataY[1]
-      feed_dict[self.inputY_flt2]     = dataY[2]
+      feed_dict[self.inputY] = dataY
     elif dSet is not None:
       batch = self.get_data(self.data[dSet+"_X"].shape[0], dSet)
-      feed_dict[self.inputX]          = batch[0]
-      feed_dict[self.inputY_quality]  = batch[1]
-      feed_dict[self.inputY_flt1]     = batch[2]
-      feed_dict[self.inputY_flt2]     = batch[3]
+      feed_dict[self.inputX] = batch[0]
+      feed_dict[self.inputY] = batch[1]
 
     else:
       print("ERROR: Must specify dSet or dataX and dataY for get_loss!!!")
@@ -655,25 +577,3 @@ class diffractionCLASS():
     else:
       self.saver.save(self.sess, fileName)
 
-"""
-  #############################################################################
-  def restoreModel(self, session, folderName, expect_exists=False, import_train_history=False):
-    print("folder",folderName)
-
-    ckpt = tf.train.get_checkpoint_state(folderName)
-    #ckpt = tf.train.get_checkpoint_state(folderName + "/" + self.FLAGS.experiment_name)
-    #print(ckpt)
-    #print(ckpt.model_checkpoint_path)
-    if ckpt:
-      self.saver.restore(session, ckpt.model_checkpoint_path)
-    else:
-      if expect_exists:
-        raise RuntimeError("ERROR: Cannot find saved checkpoint in %s" % folderName)
-      else:
-        print("Cannot find saved checkpoint at %s" % folderName)
-
-    if import_train_history:
-      pass
-
-    self.restoredModel = True
-"""

modules.py
@@ -11,6 +11,7 @@ from PIL import Image
 import sys
 import matplotlib.pyplot as plt
 from matplotlib import colors as mcolors
+from mnist import MNIST
 
 ########################
 ###  Importing Data  ###
@@ -19,39 +20,21 @@ from matplotlib import colors as mcolors
 def importData(dataDir):
 
   #####  Initialize Variables  #####
-  imgSize = 512
-  rebin   = 256
-  dataX = None 
-  dataY = None
-  quality = {
-      "blank"   : 0,
-      "noxtal"  : 1,
-      "weak"    : 2,
-      "good"    : 3,
-      "strong"  : 4}
+  mndata = MNIST(dataDir)
+  dataX_train, dataY_train = mndata.load_training()
+  dataX_test, dataY_test = mndata.load_testing()
 
-  #####  Import data  #####
-  data = pd.read_csv(dataDir + "categories1.txt", 
-      delim_whitespace=True, header=None)
+  dataX_train = np.array(dataX_train, dtype=np.float) 
+  dataY_train = np.array(dataY_train, dtype=np.int32)
+  dataX_test  = np.array(dataX_test, dtype=np.float)
+  dataY_test  = np.array(dataY_test, dtype=np.int32)
 
-  Nsamples = 30 #len(data.loc[:,0])
-  dataX = np.zeros((Nsamples, rebin, rebin, 1), dtype=np.float32)
-  dataY = np.zeros((Nsamples, 3), dtype=np.float32)
-  dataY[:,1:] = np.array((data.loc[:Nsamples-1,2:]).values, dtype=np.float32)
-  for i in range(Nsamples):
-    imgObject = Image.open(dataDir + "png/" + data.loc[i,0])
-    imgArray  = np.reshape(
-                  np.array(imgObject.getdata()).astype(np.float32),
-                  (rebin,2,rebin,-1))
-    dataX[i,:,:,0] = imgArray.mean(-1).mean(1)
+  dataX_train = np.reshape(dataX_train, (dataX_train.shape[0],28,28,1))
+  dataY_train = np.reshape(dataY_train, (-1,1))
+  dataX_test  = np.reshape(dataX_test, (dataX_test.shape[0],28,28,1))
+  dataY_test  = np.reshape(dataY_test, (-1,1))
 
-    if data.loc[i,1] in quality.keys():
-      dataY[i,0] = quality[data.loc[i,1]]
-    else:
-      print("ERROR: Cannot interpret data quality {}".format(data.loc[i,1]))
-      sys.exit()
-
-  return dataX, dataY
+  return dataX_train, dataY_train, dataX_test, dataY_test
 
 
 
@@ -90,66 +73,18 @@ Embed input into learned vector representation
 """
 class predictionClass(object):
 
-  def __init__(self, embedding_size, Nquality_classes):
+  def __init__(self, embedding_size, Nclasses):
     self.embedding_size = embedding_size
-    self.Nquality_classes = Nquality_classes
+    self.Nclasses = Nclasses
 
   def predict(self, inputX, isTraining):
     with tf.variable_scope("embedding"):
 
-      print("inp", inputX.shape.as_list())
-      conv1     = tf.contrib.layers.conv2d(inputs=inputX, 
-                    num_outputs=32, kernel_size=4)
-      print("conv1", conv1.shape.as_list())
-      conv1_BN  = tf.contrib.layers.batch_norm(conv1, 
-                    is_training=isTraining)
-      print("conv1BN", conv1_BN.shape.as_list())
-      pool1     = tf.layers.max_pooling2d(inputs=conv1_BN,
-                    pool_size=4, strides=2,
-                    name="pool1")
-      print("pool1", pool1.shape.as_list())
-      conv2     = tf.contrib.layers.conv2d(inputs=pool1,
-                    num_outputs=64, kernel_size=4)
-      print("conv2", conv2.shape.as_list())
-      conv2_BN  = tf.contrib.layers.batch_norm(conv2, 
-                    is_training=isTraining)
-      print("conv2BN", conv2_BN.shape.as_list())
-      pool2     = tf.layers.max_pooling2d(inputs=conv2_BN,
-                    pool_size=4, strides=2,
-                    name="pool2")
-      print("pool2", pool2.shape.as_list())
+      W = tf.Variable(tf.zeros([784, self.Nclasses]))
+      b = tf.Variable(tf.zeros([self.Nclasses]))
 
-      pool2Shape = pool2.shape.as_list()
-      pool2Flat = tf.reshape(pool2, [-1, pool2Shape[1]*pool2Shape[2]*pool2Shape[3]])
-      print("flat", pool2Flat.shape.as_list())
-      fullConn1 = tf.contrib.layers.fully_connected(
-                    inputs=pool2Flat,
-                    num_outputs=1024,
-                    activation_fn=tf.nn.relu,
-                    scope="FC1")
-      print("pool2", pool2.shape.as_list())
-      embedding = tf.contrib.layers.fully_connected(
-                    inputs=fullConn1,
-                    num_outputs=self.embedding_size,
-                    activation_fn=tf.nn.relu,
-                    scope="FC2")
-      print("pool2", pool2.shape.as_list())
+      x = tf.reshape(inputX, (-1,784))
+      predictions = tf.matmul(x, W) + b
 
-      qualityLogits = tf.contrib.layers.fully_connected(
-                        inputs=embedding,
-                        num_outputs=self.Nquality_classes,
-                        activation_fn=None)
-      print("qual", qualityLogits.shape.as_list())
-      flt1           = tf.contrib.layers.fully_connected(
-                        inputs=embedding,
-                        num_outputs=1,
-                        activation_fn=None)
-      print("flt1", flt1.shape.as_list())
-      flt2           = tf.contrib.layers.fully_connected(
-                        inputs=embedding,
-                        num_outputs=1,
-                        activation_fn=None)
-      print("flt2", flt2.shape.as_list())
-
-      return qualityLogits, flt1, flt2
+      return predictions
 

runNetwork.py
@@ -3,6 +3,7 @@ import model as mdl
 from modules import *
 import numpy as np
 import random
+import sys
 #from tensorflow.app.flags import DEFINE_integer as int_flag
 #from tf.app.flags import DEFINE_float as flt_flag
 #from tf.app.flags import DEFINE_string as str_flag
@@ -11,10 +12,10 @@ import random
 
 
 # High-level options
-tf.app.flags.DEFINE_string("modelName", "SSRL", "Name of the model")
+tf.app.flags.DEFINE_string("modelName", "Basic", "Name of the model")
 tf.app.flags.DEFINE_integer("gpu", 0, "Which GPU to use, if you have multiple.")
 tf.app.flags.DEFINE_string("mode", "train", "Available modes: train / show_examples / official_eval")
-tf.app.flags.DEFINE_string("experiment_name", "diffraction", "Unique name for your experiment. This will create a directory by this name in the experiments/ directory, which will hold all data related to this experiment.")
+tf.app.flags.DEFINE_string("experiment_name", "MNIST", "Unique name for your experiment. This will create a directory by this name in the experiments/ directory, which will hold all data related to this experiment.")
 tf.app.flags.DEFINE_integer("Nepochs", 0, "Number of epochs to train. 0 means train indefinitely.")
 tf.app.flags.DEFINE_integer("print_every", 1000, "Print training statues every N batches")
 tf.app.flags.DEFINE_integer("eval_every", 1, "Print training statues every N batches")
@@ -23,7 +24,7 @@ tf.app.flags.DEFINE_bool("verbose", True, "Print")
 # Hyperparameters
 tf.app.flags.DEFINE_float("dropout", 0.15, "Fraction of units randomly dropped on non-recurrent connections.")
 tf.app.flags.DEFINE_integer("batch_size", 10, "Batch size to use")
-tf.app.flags.DEFINE_integer("embedding_size", 512, "Size of vector representation.")
+tf.app.flags.DEFINE_integer("embedding_size", 256, "Size of vector representation.")
 
 # Optimization
 tf.app.flags.DEFINE_float("learning_rate", 0.001, "Learning rate.")
@@ -38,6 +39,7 @@ tf.app.flags.DEFINE_float("trainRatio", 0.70, "Ratio of the data that should be 
 tf.app.flags.DEFINE_float("valRatio", 0.15, "ratio of the data that should be used for validation")
 tf.app.flags.DEFINE_float("testRatio", 0.15, "ratio of the data that should be used for testing")
 tf.app.flags.DEFINE_integer("Nfeatures", 11, "Number of features per event") 
+tf.app.flags.DEFINE_integer("eval_batch_size", 64, "Maximum number of samples to evaluate at a time")
 
 # Saving
 tf.app.flags.DEFINE_integer("save_every", 5, "Save model parameters every N training steps")
@@ -63,46 +65,55 @@ tfConfig.gpu_options.allow_growth = True
 print("Importing Data")
 
 ###  Import all the data  ####
-dataDir = "data/"
-dataX, dataY = importData(dataDir)
-dataX -= np.mean(dataX)
-dataX /= np.var(dataX)
+dataDir = "data/mnist/"
+dataX_train, dataY_train, dataX_test, dataY_test = importData(dataDir)
 
+dataX_train -= np.mean(dataX_train)
+dataX_train /= np.var(dataX_train)
+dataX_test  -= np.mean(dataX_test)
+dataX_test  /= np.var(dataX_test)
 
-#if verbose:
-#  print("Original data X/Y shape:  {}  /  {}".format(dataX.shape, dataY.shape))
+if FLAGS.verbose:
+  print("Original data X/Y shape:  {}  /  {}"
+      .format(dataX_train.shape, dataY_train.shape))
 
 ###  Split data  ###
 data = {}
-Nevents = dataX.shape[0]
+Nevents = dataX_train.shape[0]
 randInds = np.arange(Nevents)
 random.shuffle(randInds)
-ind1 = int(np.ceil(FLAGS.trainRatio*Nevents))
-ind2 = int(np.ceil((FLAGS.trainRatio + FLAGS.valRatio)*Nevents))
-ind3 = int(np.ceil((1 - FLAGS.testRatio)*Nevents))
-data["train_X"]   = dataX[randInds[:ind1],:]
-data["val_X"]     = dataX[randInds[ind1:ind2],:]
-data["test_X"]    = dataX[randInds[ind2:],:]
-data["train_Y"]   = dataY[randInds[:ind1],:]
-data["val_Y"]     = dataY[randInds[ind1:ind2],:]
-data["test_Y"]    = dataY[randInds[ind2:],:]
+indData = int(np.ceil(FLAGS.trainRatio/(FLAGS.trainRatio + FLAGS.valRatio)*Nevents))
+inds = [1, 3, 5, 10, 9, 11, 13, 15, 17, 19]
+#data["train_X"]   = dataX_train[inds,:,:,:] #[randInds[:indData],:,:,:]
+data["train_X"]   = dataX_train[randInds[:indData],:,:,:]
+data["val_X"]     = dataX_train[randInds[indData:Nevents],:,:,:]
+data["train_Y"]   = dataY_train[randInds[:indData],:]
+#data["train_Y"]   = dataY_train[inds,:] #randInds[:indData],:]
+data["val_Y"]     = dataY_train[randInds[indData:Nevents],:]
+data["test_X"]    = dataX_test
+data["test_Y"]    = dataY_test
+
+###  Print data distribution  ###
+for i in range(10):
+  print(i, 
+      float(len(np.where(data["train_Y"] == i)[0]))/data["train_Y"].shape[0],
+      float(len(np.where(data["val_Y"] == i)[0]))/data["val_Y"].shape[0])
 
 
-diffractionNet = mdl.diffractionCLASS(FLAGS, data)
+
+
+############################
+#####  Begin Training  #####
+############################
+
+basicNet = mdl.basicNNCLASS(FLAGS, data)
 
 with tf.Session(config=tfConfig) as sess:
-  # Restore previously trained models
-
-  initializeModel(sess, diffractionNet, FLAGS.checkpoint_path, 
+  # Initialize model with new or restored parameters
+  initializeModel(sess, basicNet, FLAGS.checkpoint_path, 
       expect_exists=False)
-  #diffractionNet.restoreModel(sess, FLAGS.checkpoint_path,
-  #    import_train_history=True)
 
-  diffractionNet.train(sess)
-
-
-
-
+  basicNet.train(sess)
 
 
 